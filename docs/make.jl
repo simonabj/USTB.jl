7b634a3ed644bbba3ff68ec0c8d5c43dce48a0c6
Simon Andreas Bjørn <simon.bjorn@ellipticlabs.com>
2023-02-10 13:53:27 +0100

    fix docs devbranch

--- docs/make.jl
+++ docs/make.jl
@@ -11,7 +11,7 @@ makedocs(;
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://Dainou01.github.io/USTB.jl",
-        edit_link="master",
+        edit_link="main",
         assets=String[],
     ),
     pages=[
@@ -21,5 +21,5 @@ makedocs(;
 
 deploydocs(;
     repo="github.com/Dainou01/USTB.jl",
-    devbranch="master",
+    devbranch="main",
 )
