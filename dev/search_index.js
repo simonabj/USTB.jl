f96724d25bf2369dd0e21276afd4792e49f2c37d
Documenter.jl <documenter@juliadocs.github.io>
2023-02-10 12:55:39 +0000

    build based on 7b634a3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = USTB","category":"page"},{"location":"#USTB","page":"Home","title":"USTB","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for USTB.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [USTB]","category":"page"}]
+}
