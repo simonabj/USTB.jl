2e3620f3d55d9be50d7a1287a233f4a9748ba7ef
Simon Andreas Bjørn <simon.bjorn@ellipticlabs.com>
2023-02-20 16:59:01 +0100

    Finish work on point

--- src/uff/Point.jl
+++ src/uff/Point.jl
@@ -1,13 +1,24 @@
-import CoordinateTransformations: CartesianFromSpherical
-import Lazy: @forward
+import CoordinateTransformations: Spherical, SphericalFromCartesian
+import Unitful: m, rad
+import StaticArrays: SVector
 
-export Point
+export Point, CartesianFromPoint, PointFromCartesian
 
-Base.@kwdef struct Point
-    # UFF metadata
-    _header::Uff = Uff()
-    
-    distance::Float64  = 0.0
-    azimuth::Float64   = 0.0
-    elevation::Float64 = 0.0
+struct Point
+    distance::typeof(1.0*m)
+    azimuth::typeof(1.0*rad)
+    elevation::typeof(1.0*rad)
 end
+
+Point(r::Float64, θ::Float64, ϕ::Float64)  = Point(r*m, θ*rad, ϕ*rad)
+Point(r::Number, θ::Number, ϕ::Number) = Point(convert(Float64, r), convert(Float64, θ), convert(Float64, ϕ))
+Point(s::Spherical) = Point(s.r*m, s.θ*rad, s.ϕ*rad)
+
+CartesianFromPoint() = 
+    p -> SVector(
+    p.distance * sin(p.azimuth) * cos(p.elevation),
+    p.distance * sin(p.elevation),
+    p.distance * cos(p.azimuth)*cos(p.elevation)
+)
+
+PointFromCartesian() = p -> Point((SphericalFromCartesian())(p))
